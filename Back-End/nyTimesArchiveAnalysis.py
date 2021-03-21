3f099f32545c71640dd545f726aea3a1927038f1
Steven Zambrano <stevenzambr@gmail.com>
2021-03-21 21:35:04 +0100

    First experience creating wordcloud from real data

--- Back-End/nyTimesArchiveAnalysis.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-# Read stored data
-with open('dataFiles/nyTimesData_2021_1.json') as f:
-  data_2021_1 = json.load(f)
-
-# TODO: Data Analysis of NyTimes articles
-print(data_2021_1["response"]["docs"][0]["headline"]["main"])
